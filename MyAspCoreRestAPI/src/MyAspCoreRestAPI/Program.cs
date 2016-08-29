bcf422e1c157cb21901013be79569d9d12c8288a
Satyan <msatyan@gmail.com>
2016-08-29 00:21:06 -0500

    First Commit

---
+++ MyAspCoreRestAPI/src/MyAspCoreRestAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿// https://www.youtube.com/watch?v=e2qZvabmSvo
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace MyAspCoreRestAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
