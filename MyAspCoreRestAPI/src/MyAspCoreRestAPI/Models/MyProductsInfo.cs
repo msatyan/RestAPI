e00cdc99b78a32c751837b245515485cb0d8c9c6
Satyan <msatyan@gmail.com>
2016-09-05 14:52:44 -0500

    Models

---
+++ MyAspCoreRestAPI/src/MyAspCoreRestAPI/Models/MyProductsInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyAspCoreRestAPI.Models
+{
+    public class MyProductsInfo
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(30)]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Display(Name = "Product Price")]
+        public int Price { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string ContactEmail { get; set; }
+    }
+}
+
